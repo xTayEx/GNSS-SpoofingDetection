40eceeaf9ed9a006161290e1045cdae34bf70716
xtayex <75450574@qq.com>
2022-03-16 09:05:52 +0800

    checkout train.py from multi_sequence

--- train.py
+++ train.py
@@ -1,73 +1,115 @@
+import os
 import pandas as pd
 import math
 import numpy as np
+import argparse
+from tqdm import tqdm
 from sklearn.preprocessing import MinMaxScaler
 from keras.models import Sequential
-from keras.layers import Dense, Masking
+from keras.layers import Dense
+from keras.layers import Masking
 from keras.layers import LSTM
+from keras.preprocessing.sequence import pad_sequences
 from sklearn.metrics import mean_squared_error
 import matplotlib.pyplot as plt
 from keras import optimizers
 import tensorflow as tf
+import requests
+from tqdm import tqdm
+from time import time as timing
 gpus = tf.config.experimental.list_physical_devices('GPU')
 for gpu in gpus:
     tf.config.experimental.set_memory_growth(gpu, True)
 
 
+def create_model(input_shape):
+    model = Sequential()
+    model.add(LSTM(50, input_shape=input_shape))
+    model.add(Dense(1))
+    model.compile(loss='mae', optimizer='adam')
+    return model
+
+
+def process_data(csv_file_path):
+    df = pd.read_csv(csv_file_path)
+    values = df.to_numpy()
+    times = values[:, -1]
+    distance = values[:, -2]
+    scaler = MinMaxScaler(feature_range=(0, 1))
+    X, y = scaler.fit_transform(values[:, :-2]), distance
+    X = X.reshape((X.shape[0], 1, X.shape[1]))
+    # print(X.shape)
+    # print(X)
+    return X, y
+
+
 def train(model, train_csv_file_path, test_csv_file_path):
-    pass
+    train_X, train_y = process_data(train_csv_file_path)
+    test_X, test_y = process_data(test_csv_file_path)
+    
+    model.fit(train_X, train_y, epochs=1, batch_size=64, validation_data=(test_X, test_y), verbose=2, shuffle=False)
+
+    return model    
+
+
+def get_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--shutdown', action='store_true')
+    parser.add_argument('--epochs', type=int, default=20)
+    parser.add_argument('--data_root', type=str, default='/root/autodl-nas/')
+    parser.add_argument('--test_csv_path', type=str, default="/root/99c94dc769b5d96e|2018-07-10--10-01-44.csv")
+
+    args = parser.parse_args()
+    return args
+
+
+def main(args):
+    epochs = args.epochs
+    data_root = args.data_root
+    test_csv_path = args.test_csv_path
+    chunks = os.listdir(data_root)
+    
+    model = create_model((1, 3))
+    for epoch in tqdm(range(epochs)):
+        for chunk in chunks:
+            chunk_path = os.path.join(data_root, chunk)
+            csv_files = filter(lambda f: f.split('.')[-1] == 'csv', os.listdir(chunk_path))
+            for csv in csv_files:
+                print(f'csv file is {csv}')
+                model = train(model, os.path.join(data_root, chunk, csv), test_csv_path)
+    
+    model.save('lstm_all_data.h5')
+
+    # plot history
+    # plt.plot(history.history['loss'], label='train')
+    # plt.plot(history.history['val_loss'], label='test')
+    # plt.legend()
+    # plt.savefig('train_result.png')
 
 
 if __name__ == '__main__':
+    args = get_args()
+    print(args)
+    try:
+        main(args)
+    except Exception as e:
+        resp = requests.post('https://www.autodl.com/api/v1/wechat/message/push', json={
+            'token': '9ae892707a79',
+            'title': 'Exception',
+            'name': 'GNSS Spoofing Detection',
+            'content': str(e)
+        })
 
-    train_CSV_FILE_PATH = '/root/autodl-nas/Chunk_01/b0c9d2329ad1606b|2018-08-02--08-34-47.csv'
-    test_CSV_FILE_PATH = '/root/autodl-nas/Chunk_01/b0c9d2329ad1606b|2018-08-01--21-13-49.csv'
-    train_df = pd.read_csv(train_CSV_FILE_PATH)
-    test_df = pd.read_csv(test_CSV_FILE_PATH)
-    train_values = train_df.to_numpy()
-    train_times = train_values[:, -1]
-    train_distance = train_values[:, -2]
-    test_values = test_df.to_numpy()
-    test_times = test_values[:, -1]
-    test_distance = test_values[:, -2]
-    # 将输入特征归一化
-    scaler = MinMaxScaler(feature_range=(0, 1))
-    train_X, train_y = scaler.fit_transform(train_values[:, :-2]), train_distance
-    test_X, test_y = scaler.fit_transform(test_values[:, :-2]), test_distance
-    # # 将四分之三作为训练集
-    # train_len = len(times)
-    # train = values[:train_len, :]
-    # test = values[train_len:, :]
-    # 划分输入（CAN_speed,steering_angel, acceleration_forward）输出（distance)
-    # train_X, train_y = train, distance[:train_len]
-    # test_X, test_y = test, distance[train_len:]
-    # 将输入（X）改造为LSTM的输入格式，即[samples, timesteps, features]
-    train_X = train_X.reshape((train_X.shape[0], 1, train_X.shape[1]))
-    test_X = test_X.reshape((test_X.shape[0], 1, test_X.shape[1]))
-
-    # 设计网络
-    model = Sequential()
-    model.add(LSTM(50, input_shape=(train_X.shape[1], train_X.shape[2],)))
-    model.add(Dense(1))
-    # 设置学习率等参数
-    # adam = optimizers.Adam(lr=0.01, beta_1=0.9, beta_2=0.999, epsilon=1e-08)
-    model.compile(loss='mae', optimizer='adam')
-    # fit network
-    history = model.fit(train_X, train_y, epochs=100, batch_size=64, validation_data=(test_X, test_y), verbose=2, shuffle=False)
-    model.save('gnss_spoofing_detect.h5')
-    # full_X = values[:, :3]
-    # full_X = full_X.reshape((full_X.shape[0], 1, full_X.shape[1]))
-    train_yhat = model.predict(train_X)[:, 0]
-    test_yhat = model.predict(test_X)[:, 0]
-    rmse = math.sqrt(mean_squared_error(test_yhat, test_y))
-    print('Test RMSE: %.3f' % rmse)
-    # plot history
-    plt.plot(history.history['loss'], label='train')
-    plt.plot(history.history['val_loss'], label='test')
-    # plt.plot(times, yhat, label='prediction')
-    # plt.plot(times, distance, label="ground_truth")
-    # plt.title('Comparison between truth and prediction', fontsize=18)
-    # plt.xlabel('Boot time (s)', fontsize=18)
-    # plt.ylabel('Distance travelled during single timestamp (m) ', fontsize=12)
-    plt.legend()
-    plt.show()
+    else:
+        success_content = 'Done!'
+        if args.shutdown:
+            success_content += ' Server will be shutdown in 1 min...'
+        
+        resp = requests.post('https://www.autodl.com/api/v1/wechat/message/push', json={
+            'token': '9ae892707a79',
+            'title': 'Success',
+            'name': 'GNSS Spoofing Detection',
+            'content': 'Done'
+        })
+        if args.shutdown:
+            os.system('shutdown +1')
