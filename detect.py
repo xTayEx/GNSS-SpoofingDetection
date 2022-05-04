30c1bfbef5b06a05f047dc9748a0f2d92f0408fa
xtayex <75450574@qq.com>
2022-05-04 21:24:30 +0800

    generate log

--- detect.py
+++ detect.py
@@ -9,6 +9,8 @@ import logging
 from time import time as timing
 from sklearn.metrics import mean_absolute_error
 from sklearn.preprocessing import MinMaxScaler
+from ctypes import CDLL
+from ctypes import c_char_p, c_void_p, create_string_buffer, byref
 
 gpus = None
 logging.basicConfig(filename='debug.log', level=logging.WARNING)
@@ -24,6 +26,7 @@ def average(seq, total=0.0):
 def get_args():
     parser = argparse.ArgumentParser()
 
+
 def detect_init():
     global gpus
     gpus = tf.config.experimental.list_physical_devices('GPU')
@@ -32,35 +35,38 @@ def detect_init():
 
 
 def detect():
+    log_generator = CDLL('./loggen.so')
     GNSS_ERROR = 1.5
     LSTM_PREDICT_ERROR = 0.058002
-    SPOOFING_THRESHOLD = GNSS_ERROR + LSTM_PREDICT_ERROR 
+    SPOOFING_THRESHOLD = GNSS_ERROR + LSTM_PREDICT_ERROR
 
-    csv_file_path = 'test_spoofing.csv' 
+    csv_file_path = 'test_spoofing.csv'
     df = pd.read_csv(csv_file_path)
     values = df.to_numpy()
     times = values[:, -1]
     distance = values[:, -2]
-    model = tf.keras.models.load_model('lstm_all_data.h5')
+    model = tf.keras.models.load_model('gnss_spoofing_detect.h5')
     test_X = values[:, :3]
 
     scaler = MinMaxScaler(feature_range=(0, 1))
     test_X = scaler.fit_transform(test_X)
     test_X = test_X.reshape((test_X.shape[0], 1, test_X.shape[1]))
-    
+
     test_y = distance
     yhat = model.predict(test_X)[:, 0]
-    
+
+    output = create_string_buffer(b'\000' * 440)
     for idx, y_predict_point in enumerate(yhat):
         if abs(y_predict_point - distance[idx]) > SPOOFING_THRESHOLD:
-            print('>>> WARNING! <<<')
-            print(f'GNSS SPOOFING OCCURED AT {idx}')
-            logging.warning(f'[WARN] GNSS SPOOFING OCCURED AT {idx}. distance: {distance[idx]}, y_p: {y_predict_point}, diff: {distance[idx] - y_predict_point}')
+            logging.warning(f'GNSS SPOOFING OCCURED AT {idx}.distance:{distance[idx]},y_p:{y_predict_point},diff:{distance[idx]-y_predict_point}')
+            log_size = log_generator.logtrans(c_char_p(f'[WARN] GNSS SPOOFING OCCURED AT {idx}.distance:{distance[idx]},y_p:{y_predict_point},diff:{distance[idx]-y_predict_point}'.encode()), byref(output))
+            print(output.value)
         else:
-            logging.warning(f'{idx}. distance: {distance[idx]}, y_p: {y_predict_point}, diff: {distance[idx] - y_predict_point}')
+            logging.warning(f'{idx}.distance:{distance[idx]},y_p:{y_predict_point},diff:{distance[idx] - y_predict_point}')
+            log_size = log_generator.logtrans(c_char_p(f'[INFO] No Spoofing'.encode()), byref(output))
+            print(output.value)
 
 
-    
 if __name__ == '__main__':
     detect_init()
     detect()
