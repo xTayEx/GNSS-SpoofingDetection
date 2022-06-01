083e288922ff9f5d08d4eb67145e3ea06734d144
xtayex <75450574@qq.com>
2022-06-01 23:48:45 +0800

    json payload

--- detect.py
+++ detect.py
@@ -1,5 +1,6 @@
 import argparse
 import math
+import json
 
 import matplotlib.pyplot as plt
 import pandas as pd
@@ -7,10 +8,9 @@ import numpy as np
 import tensorflow as tf
 import logging
 from time import time as timing
-from sklearn.metrics import mean_absolute_error
 from sklearn.preprocessing import MinMaxScaler
 from ctypes import CDLL
-from ctypes import c_char_p, c_void_p, c_int, create_string_buffer, byref
+from ctypes import c_char_p, c_int, create_string_buffer, byref
 
 gpus = None
 logging.basicConfig(filename='debug.log', level=logging.WARNING)
@@ -29,7 +29,7 @@ def get_args():
 
 def detect_init():
     global gpus
-    gpus = tf.config.experimental.list_physical_devices('GPU')
+    gpus = tf.confg.experimental.list_physical_devices('GPU')
     for gpu in gpus:
         tf.config.experimental.set_memory_growth(gpu, True)
 
@@ -55,16 +55,34 @@ def detect():
     test_y = distance
     yhat = model.predict(test_X)[:, 0]
 
-    output = create_string_buffer(b'\000' * 440)
+    output_buf = create_string_buffer(b'\000' * 440)
     for idx, y_predict_point in enumerate(yhat):
         if abs(y_predict_point - distance[idx]) > SPOOFING_THRESHOLD:
+            payload = {
+                'level': 'WARNING',
+                'msg': 'SPOOFING DETECTED',
+                'occur_index': idx,
+                'distance': distance[idx],
+                'y_pre': y_predict_point,
+                'diff': distance[idx] - y_predict_point
+            }
+            payload_json_str = json.dumps(payload)
             logging.warning(f'GNSS SPOOFING OCCURED AT {idx}.distance:{distance[idx]},y_p:{y_predict_point},diff:{distance[idx]-y_predict_point}')
-            log_size = log_generator.logtrans(c_char_p(f'[WARN] GNSS SPOOFING OCCURED AT {idx}.distance:{distance[idx]},y_p:{y_predict_point},diff:{distance[idx]-y_predict_point}'.encode()), byref(output))
+            log_size = log_generator.logtrans(c_char_p(payload_json_str.encode()), byref(output_buf))
         else:
+            payload = {
+                'level': 'INFO',
+                'msg': 'NO SPOOFING',
+                'occur_index': idx,
+                'distance': distance[idx],
+                'y_pre': y_predict_point,
+                'diff': distance[idx] - y_predict_point
+            }
+            payload_json_str = json.dumps(payload)
             logging.warning(f'{idx}.distance:{distance[idx]},y_p:{y_predict_point},diff:{distance[idx] - y_predict_point}')
-            log_size = log_generator.logtrans(c_char_p(f'[INFO] No Spoofing'.encode()), byref(output))
+            log_size = log_generator.logtrans(c_char_p(payload_json_str.encode()), byref(output_buf))
 
-        log_generator.showHexIDPSlog(byref(output), c_int(log_size))
+        log_generator.showHexIDPSlog(byref(output_buf), c_int(log_size))
 
 
 if __name__ == '__main__':
